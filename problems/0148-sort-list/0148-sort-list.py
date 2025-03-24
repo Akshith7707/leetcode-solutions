a9a2640518886aea864a018ebb666f60a95413ab
Ponna Akshith Reddy <akshithreddy.ponna@gmail.com>
2025-03-24 14:34:34 +0000

    LeetSyncMaster - Sort List (Python3)

---
+++ problems/0148-sort-list/0148-sort-list.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        slow,fast=head,head.next
+        while fast and fast.next:
+            slow,fast=slow.next,fast.next.next
+        #splittinggg
+        temp=slow.next
+        slow.next=None
+        left,right=self.sortList(head),self.sortList(temp)
+        dummy=ListNode()
+        curr=dummy
+        while left and right:
+            if left.val<=right.val:
+                curr.next=left
+                left=left.next
+            else:
+                curr.next=right
+                right=right.next
+            curr=curr.next
+        curr.next=left or right
+        return dummy.next
